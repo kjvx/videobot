35df71a5a6bde8f86239b5b8a475131b17a3ca3a
kjvx <110008132+kjvx@users.noreply.github.com>
2022-09-28 17:45:47 -0400

    video 2

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ intents=discord.Intents.all()
 
 bot = discord.Bot(command_prefix=commands.when_mentioned_or("$"), description=description, intents=intents)
 
+
+filtered_words = ["blue", "red", "green", "yellow"]
+@bot.event
+async def on_message(message):
+    for word in filtered_words:
+        if word in message.content:
+            print(f"message deleted! {message.content}")
+            await message.delete()
+
+
+
+
+
+
+
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is ready ")
@@ -24,6 +40,45 @@ async def on_ready():
 @bot.slash_command()
 async def ping(ctx, name):
     await ctx.respond(f"Pong! our bots ping is {bot.latency} seconds,  your name is {name}")
+
+
+
+@bot.slash_command()
+async def calculate(ctx, number1, operator, number2):
+    
+    def add(num1, num2):
+        return num1 + num2
+
+    def multiply(num1, num2):
+        return num1 * num2
+
+    def divide(num1, num2):
+        return num1 / num2
+
+    def subtract(num1, num2):
+        return num1 - num2
+
+    
+    if operator == "+":
+        result = add(int(number1), int(number2))
+
+
+        await ctx.respond(f"Done! you number is {result}")
+
+    elif operator == "-":
+        result = subtract(int(number1), int(number2))
+        await ctx.respond(f"Done! you number is {result}")
+
+    elif operator == "*":
+        result = multiply(int(number1), int(number2))
+        await ctx.respond(f"Done! you number is {result}")
+
+    elif operator == "/":
+        result = divide(int(number1), int(number2))
+        await ctx.respond(f"Done! you number is {result}")
+    
+
+
 
 
 
